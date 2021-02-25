80ec90e7f0bb646a493f6d980c9354403214a260
ANA SILVA <ana.silva2010@fatec.sp.gov.br>
2021-02-25 14:30:22 -0300

    Day4

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,23 +4,33 @@ import { CompletedChallenges } from '../components/CompletedChallenges'
 import { Countdown } from '../components/Countdown'
 import ExperienceBar from '../components/ExperienceBar'
 import { Profile } from '../components/Profile'
+import { CountdownProvider } from '../contexts/CountdownContext'
 
 import styles from '../styles/pages/Home.module.css'
 
 export default function Home() {
   return (
     <div className={styles.container}>
-        <ExperienceBar />
+      <Head>
+        <title>Inicio | move.it</title>
+      </Head>
+
+      <ExperienceBar />
+
+      <CountdownProvider>
+
         <section>
           <div>
-          <Profile />
-          <CompletedChallenges/>
-          <Countdown />
+            <Profile />
+            <CompletedChallenges />
+            <Countdown />
           </div>
           <div>
             <ChallengeBox />
           </div>
         </section>
+      </CountdownProvider>
+
     </div>
   )
 }
